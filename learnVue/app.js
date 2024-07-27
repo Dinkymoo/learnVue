782ff81fed8d780d89a337fef4bee804ce7fa538
Danielle van Graan <daniellefunnygraan@gmail.com>
2024-07-27 23:44:45 +0200

    starting with events

--- learnVue/app.js
+++ learnVue/app.js
@@ -1,21 +1,17 @@
 const app = Vue.createApp({
-  //shorthand function
   data() {
     return {
-      //always an object
-      courseGoalA: "Finish the Vue course!",
-      courseGoalB: "<h2 style='color:white'>Learn more Vue!</h2>",
-      vueLink: "http://vueja.org",
+      counter: 0,
     };
   },
   methods: {
-    ouputGoal() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5)
-      return this.courseGoalA;
-      else return this.courseGoalB;
+    addToCounter() {
+      this.counter++;
+    },
+    removeFromCounter() {
+      this.counter--;
     },
   },
 });
 
-app.mount("#user-goal");
+app.mount('#events');
